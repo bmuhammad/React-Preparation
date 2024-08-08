d15bca255b4435d0261136d16df9b353992912ef
Brian <briancmuhammad@yahoo.com>
2024-08-08 16:23:09 -0700

    added yarn and initial home and post pages with axios call to api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/"element={<Home />}></Route>
+          <Route path=":id" element={<Posts />}></Route>  
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+//Dynamic routs use path=":id"
