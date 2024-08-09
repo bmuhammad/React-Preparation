8f58684207930d25f149a6020f173b1a5013287e
Brian <briancmuhammad@yahoo.com>
2024-08-08 17:23:00 -0700

    add junior coding example for search for id from button click. added code for back button

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 const Posts = () => {
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState();
   const [searchId, setSearchId] = useState(id);
 
+  function onSearch() {
+    fetchPosts(searchId);
+  }
 
-function onSearch() {
-fetchPosts(searchId);
-}
-
-async function fetchPosts(userId) {
+  async function fetchPosts(userId) {
+    setLoading(true);
     const { data } = await axios.get(
       `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`
     );
@@ -21,57 +21,52 @@ async function fetchPosts(userId) {
     setLoading(false);
   }
 
+  function onSearchKeyPress(key) {
+    if (key === "Enter") {
+      onSearch();
+    }
+  }
+
   useEffect(() => {
-   
     fetchPosts();
   }, []);
 
   return (
     <>
-   
-        <div>
-          <div className="post__search">
+      <div>
+        <div className="post__search">
+          <Link to="/">
             <button>← Back</button>
-            <div className="post__search--container">
-              <label className="post__search--label">Search by Id</label>
-              <input type="number" value={searchId} onChange={(event) => setSearchId(event.target.value)}
-              onKeyPress={(event) => {
-                if (event.key === 'Enter'){
-                    onSearch();
-                }
-              }} />
-              <button onClick={() => onSearch()}>Enter</button>
-            </div>
+          </Link>
+          <div className="post__search--container">
+            <label className="post__search--label">Search by Id</label>
+            <input
+              type="number"
+              value={searchId}
+              onChange={(event) => setSearchId(event.target.value)}
+              onKeyPress={(event) => onSearchKeyPress(event.key)}
+            />
+            <button onClick={() => onSearch()}>Enter</button>
           </div>
-{
-    loading
-    ? 
-
-        new Array(10).fill(0).map((element, index) => (
-            <div className="post" key ={index}>
-            <div className="post__title">
-              <div className="post__title--skeleton"></div>
-            </div>
-            <div className="post__body">
-              <p className="post__body--skeleton"></p>
-            </div>
-          </div>
-        ))
-
-    : (
-        posts.map((post) => (
-            <div className="post" key={post.id}>
-              <div className="post__title">{post.title}</div>
-              <p className="post__body">{post.body}</p>
-            </div>
-          ))
-    )
-}
-         
-        
-          
         </div>
-    
+        {loading
+          ? new Array(10).fill(0).map((element, index) => (
+              <div className="post" key={index}>
+                <div className="post__title">
+                  <div className="post__title--skeleton"></div>
+                </div>
+                <div className="post__body">
+                  <p className="post__body--skeleton"></p>
+                </div>
+              </div>
+            ))
+          : posts.map((post) => (
+              <div className="post" key={post.id}>
+                <div className="post__title">{post.title}</div>
+                <p className="post__body">{post.body}</p>
+              </div>
+            ))}
+      </div>
     </>
   );
 };
